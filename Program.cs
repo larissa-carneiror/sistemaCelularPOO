d738057cda805404ee7697e25c928830be6c8ad4
Larissa Rodrigues <larissa-rodriguesc@hotmail.com>
2023-12-30 13:54:17 -0300

    desafio finalizado

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia: ");
+Smartphone nokia = new Nokia(numero: "01234", modelo: "Tijolo", imei: "abc123", memoria: 32);
+nokia.Ligar();
+nokia.InstalarAplicativo("Instagram", modelo: nokia.Modelo);
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone: ");
+Smartphone iphone = new Iphone(numero: "56789", modelo: "15", imei: "bcd456", memoria: 64);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Whatsapp", modelo: iphone.Modelo);
